df95008c3f34490b779eda0ae599a16c499fc799
Pablo Panero <ppanero@cern.ch>
2019-11-07 16:53:27 +0100

    global: update NU copyright

--- invenio_cli/utils.py
+++ invenio_cli/utils.py
@@ -2,8 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2019 CERN.
-# Copyright (C) 2019 Northwestern University,
-#                    Galter Health Sciences Library & Learning Center.
+# Copyright (C) 2019 Northwestern University.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
