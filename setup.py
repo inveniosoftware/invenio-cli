5a302ac8356b5ed01a42f027877d6e3c00f09942
Pablo Panero <ppanero@cern.ch>
2019-11-08 10:15:20 +0100

    global: remove python 2 support, add 3.6

--- setup.py
+++ setup.py
@@ -98,10 +98,8 @@ setup(
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 3 - Alpha',
     ],
 )
