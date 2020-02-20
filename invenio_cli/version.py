da170dcec9fae691a47e7ba5773aa61957141794
Guillaume Viger <fenekku@fenekku.com>
2020-02-20 10:26:30 -0600

    release: v0.10.0

--- invenio_cli/version.py
+++ invenio_cli/version.py
@@ -12,6 +12,4 @@ This file is imported by ``invenio_cli.__init__``,
 and parsed by ``setup.py``.
 """
 
-from __future__ import absolute_import, print_function
-
-__version__ = '1.0.0a9'
+__version__ = '0.10.0'
