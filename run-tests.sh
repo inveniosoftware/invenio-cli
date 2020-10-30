6068cbac8fd6e0ab7de9d14034511910c0a34641
Pablo Panero <pablo.panero@cern.ch>
2020-10-30 13:25:03 +0100

    workflows: add tests and remove travis file

--- run-tests.sh
+++ run-tests.sh
@@ -7,6 +7,6 @@
 # Invenio-Cli is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-python -m check_manifest --ignore ".travis-*" && \
+python -m check_manifest --ignore ".*-requirements.txt" && \
 python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
 python -m pytest
