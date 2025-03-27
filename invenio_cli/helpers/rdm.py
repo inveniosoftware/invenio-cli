94292ad5459bb3267bbf1bf5390b45c8388e571b
Karl-Ulrich Krägelin <k.kraegelin@uni-muenster.de>
2025-03-27 13:31:07 +0100

    rdm: enable pyproject.toml anlongside Pipfile.lock for rdm version check

--- invenio_cli/helpers/rdm.py
+++ invenio_cli/helpers/rdm.py
@@ -10,19 +10,33 @@
 
 import re
 
-from pipfile import Pipfile
+from ..helpers.cli_config import CLIConfig
 
 
 def rdm_version():
     """Return the latest RDM version."""
-    parsed = Pipfile.load(filename="./Pipfile")
+    cliConfig = CLIConfig()
+    if (cliConfig.project_path / "Pipfile").is_file():
+        depfile = cliConfig.project_path / "Pipfile"
+    elif (cliConfig.project_path / "pyproject.toml").is_file():
+        depfile = cliConfig.project_path / "pyproject.toml"
 
-    groups = re.search(
-        r"[0-9]*\.[0-9]*\.[0-9]*",
-        parsed.data["default"].get("invenio-app-rdm", {}).get("version", ""),
+    match = re.search(
+        r"invenio-app-rdm.*?==([\d.]+(?:b\d+)?(?:\.dev\d+)?)",
+        depfile.read_text(),
     )
 
-    if groups:
-        return [int(v) for v in groups.group(0).split(".")]
+    if match:
+        print(match.group(1))
+        apprdmversion = []
+        for v in match.group(1).split("."):
+
+            try:
+                vs = int(v)
+                apprdmversion.append(vs)
+            except ValueError:
+                apprdmversion.append(v)
+        print("Invenio RDM version: ", apprdmversion)
+        return apprdmversion
     else:
         return None
